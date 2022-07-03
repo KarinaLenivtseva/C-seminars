911625bb7347833c2800460b4c680a432e0be2e9
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-07-03 12:54:39 +0300

    Task 1 completed

---
+++ Desktop/GB_Seminar/Seminar2/Task1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(a);
+Console.WriteLine("Вторая цифра этого числа "+stringNumber[1]);
