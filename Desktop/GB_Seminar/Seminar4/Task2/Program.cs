44d9350a62374983db4b13c140ad0f0b426ebe2d
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-07-10 17:13:31 +0300

    Task 2 finished

---
+++ Desktop/GB_Seminar/Seminar4/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Enter number: ");
+int EnteredNumber = Convert.ToInt32(Console.ReadLine());
+
+int ResultOfSum(int EnteredNumber){
+    int Counter = Convert.ToString(EnteredNumber).Length;
+    int research = 0;
+    int result = 0;
+    for (int i = 0; i < Counter; i++){
+        research = EnteredNumber - EnteredNumber % 10;
+        result = result + (EnteredNumber - research);
+        EnteredNumber = EnteredNumber / 10;
+    }
+    return result;
+}
+
+int FinalResult = ResultOfSum(EnteredNumber);
+Console.WriteLine("Sum of numerals is: " + FinalResult);
