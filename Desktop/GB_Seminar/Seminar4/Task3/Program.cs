55172f4e8515477e99534fe307bc72433a46895f
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-07-11 15:53:42 +0300

    Task 3 completed

---
+++ Desktop/GB_Seminar/Seminar4/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] CreateRandomArray()
+{
+    int[] RandomArray = new int[8];
+    for (int i = 0; i < 8; i++)
+{
+    RandomArray[i] = new Random().Next(0, 10);
+    Console.Write(RandomArray[i] + " ");
+}
+return RandomArray;
+}
+int[] myArray = CreateRandomArray();
