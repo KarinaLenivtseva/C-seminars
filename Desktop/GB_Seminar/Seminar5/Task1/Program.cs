e37078c33d1bde28fba43b75f4f7e905ac58b364
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-07-11 16:59:00 +0300

    task 1 finished

---
+++ Desktop/GB_Seminar/Seminar5/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// //Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] number = new int[10];
+
+void Body(int[] array, int min, int max){
+  for (int i = 0; i<array.Length; i++ ){
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void Component(int[] array){
+    for (int i = 0; i<array.Length; i++ ){
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+
+int PositiveResult(int[] array){
+    int amount = 0;
+    for (int i = 0; i<array.Length; i++ ){
+    if (array[i] % 2 == 1)
+    {
+      amount++;
+    }
+  }
+  return amount;
+}
+
+Body(number, 100, 1000);
+Component(number);
+Console.WriteLine();
+
+int amount = PositiveResult(number);
+Console.WriteLine($"Quantity of positive numbers in mass is: {amount}");
