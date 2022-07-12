a8fb46531d2b67383f6974c61244915c1bda6f0f
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-07-12 15:11:50 +0300

    Task 2 finished

---
+++ Desktop/GB_Seminar/Seminar5/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Write($"Please set the amount of elements of mass: ");
+int amount = Convert.ToInt32(Console.ReadLine()); 
+
+int numbers(int amount, int min, int max)
+  {
+  int[] randomNumbers = new int[amount];
+  int Odds = 0;
+  Console.Write("Mass: ");
+
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+      Console.Write(randomNumbers[i] + " ");
+      if (i % 2 != 1)
+      {
+        Odds = Odds + randomNumbers[i];
+      }
+    }
+  return Odds;
+  }
+
+int randomNumbers =  numbers(amount, 1, 10);
+
+Console.WriteLine($"\n The sum of the elements on the odd positions: {randomNumbers}");
