6fff115038e773f5d57e82b099705ea7be53f06a
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-07-12 15:40:19 +0300

    Task 3 is done

---
+++ Desktop/GB_Seminar/Seminar5/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double[] RealNumbers = new double[10];
+  for (int i = 0; i < RealNumbers.Length; i++ )
+  {
+    RealNumbers[i] = new Random().Next(-100, 100);
+    Console.Write(RealNumbers[i] + " ");
+  }
+double MaxNum = RealNumbers[0];
+double MinNum = RealNumbers[0];
+  for (int i = 1; i < RealNumbers.Length; i++)
+  {
+    if (MaxNum < RealNumbers[i])
+    {
+      MaxNum = RealNumbers[i];
+    }
+        if (MinNum > RealNumbers[i])
+    {
+      MinNum = RealNumbers[i];
+    }
+  }
+  double result = MaxNum - MinNum;
+  Console.WriteLine($"\n The difference between max {MaxNum} and min {MinNum} elements is {result}");
