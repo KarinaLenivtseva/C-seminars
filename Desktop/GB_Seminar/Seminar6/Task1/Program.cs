a97dfa850d41c6bc8ca6bcc9fce2e0a79f62b860
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-07-16 19:48:16 +0300

    Task 1 is done

---
+++ Desktop/GB_Seminar/Seminar6/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.WriteLine ("Введите желаемое количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massive = new int[m];
+
+void Numbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    massive[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int match(int[] massive)
+{
+  int count = 0;
+  for (int i = 0; i < massive.Length; i++)
+  {
+    if(massive[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+Numbers(m);
+
+Console.WriteLine($"Чисел больше 0 было введено {match(massive)}");
